f4130d52054bdd7b7c60682f1851046768e27600
alvan <alvan.ghon@gmail.com>
2023-03-08 12:49:04 +0700

    add handle data and implemenatation read from WEBApi for hotel

---
+++ Realta.Frontend/Pages/Hotels/Hotels.Razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Realta.Contract.Models.v1.Hotels;
+using Realta.Frontend.HttpRepository;
+
+namespace Realta.Frontend.Pages.Hotels
+{
+    public partial class Hotels
+    {
+        [Inject]
+        public IHotelsHttpRepository? HotelRepo { get; set; }
+        public List<HotelsDto> HotelList { get; set; } = new List<HotelsDto>();
+        protected override async Task OnInitializedAsync()
+        {
+            HotelList = await HotelRepo.GetHotels(); 
+        }
+    }
+}
--- Realta.Frontend/Program.cs
+++ Realta.Frontend/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Realta.Frontend;
+using Realta.Frontend.HttpRepository;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7068/api/") });
+
+//Add Service for call implementation
+builder.Services.AddScoped<IHotelsHttpRepository, HotelsHttpRepository>();
 
 await builder.Build().RunAsync();
