ea5be1bd7a2accf380948e435b185e4a85242bb3
ndicz <hnrmanesi@gmail.com>
2023-03-09 09:18:01 +0700

    Berhasil Get From DBCONTEXT UHUY

---
+++ Realta.Frontend/HttpRepository/IRestoMenusHttpRepository.cs
@@ -0,0 +1,9 @@
+﻿using Realta.Contract.Models;
+
+namespace Realta.Frontend.HttpRepository
+{
+    public interface IRestoMenusHttpRepository
+    {
+        Task<List<RestoMenusDto>> GetProducts();
+    }
+}
--- Realta.Frontend/Program.cs
+++ Realta.Frontend/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Realta.Frontend;
+using Realta.Frontend.HttpRepository;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7068/api/") });
+builder.Services.AddScoped<IRestoMenusHttpRepository, RestoMenusHttpRepository>();
 
 await builder.Build().RunAsync();
